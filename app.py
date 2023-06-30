c34839c9bc1ec8acc568408781b33f725b93b0a0
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-06-30 11:56:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,12 @@ if "messages" not in st.session_state:
         {
             "role": "assistant",
             "content": """
-            I'm here to help you learn about and create Wardley Maps. Here are some options for getting started: 1. Learn: To learn about the components and concepts of a Wardley Map, type "Learn". 2. Vocabulary: To get a list of common Wardley Map terms and their definitions, type "Vocabulary". 3. Create: To create your own Wardley Map with step-by-step guidance, type "Create". If you have any specific questions or need clarification on any aspect of Wardley Mapping, feel free to ask.            """
+            I'm here to help you learn about and create Wardley Maps. Here are some options for getting started:
+            1. Learn: To learn about the components and concepts of a Wardley Map, type "Learn".
+            2. Vocabulary: To get a list of common Wardley Map terms and their definitions, type "Vocabulary".
+            3. Create: To create your own Wardley Map with step-by-step guidance, type "Create".
+            If you have any specific questions or need clarification on any aspect of Wardley Mapping, feel free to ask.
+            """
         })
 
 for message in st.session_state.messages:
