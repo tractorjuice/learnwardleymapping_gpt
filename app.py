f541aadd39e9b68cc2987cefed7646a6de0d8c63
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-06-30 11:45:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 #MODEL = "gpt-3.5-turbo-0613"
 #MODEL = "gpt-3.5-turbo-16k"
 #MODEL = "gpt-3.5-turbo-16k-0613"
-MODEL = "gpt-4"
-#MODEL = "gpt-4-0613"
+#MODEL = "gpt-4"
+MODEL = "gpt-4-0613"
 #MODEL = "gpt-4-32k-0613"
 
 st.set_page_config(page_title="Learn Wardley Mapping Bot")
