48542958a5a7c46432cef1387c424789a00d44e5
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-11-06 23:38:46 +0000

    Update app.py

--- app.py
+++ app.py
@@ -67,15 +67,14 @@ st.session_state.run = client.beta.threads.runs.create(
 )
 st.write("Run 1: ", st.session_state.run)
 
-thread_messages = openai.beta.threads.messages.list(st.session_state.thred)
-st.write(thread_messages.data)
-
-
 run = client.beta.threads.runs.retrieve(
   thread_id=st.session_state.thred.id,
   run_id=st.session_state.run.id
 )
 st.write("Run 2: ", run)
+
+thread_messages = openai.beta.threads.messages.list(st.session_state.thred)
+st.write("Messages: ", thread_messages.data)
 
 messages = client.beta.threads.messages.list(
   thread_id=st.session_state.thred.id
