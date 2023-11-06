23ce08433f52dca7160720a03918ba3bbbd0ab6a
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-11-06 22:57:25 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,56 +1,57 @@
 import streamlit as st
 import openai
 
-# Set your OpenAI API key here
+# Set your OpenAI API key here using Streamlit secrets
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 # Define the initial instructions for the assistant
 initial_instructions = "You are a helpful assistant."
 
-# Create a sidebar for control settings
-st.sidebar.title("Control Settings")
-model_name = st.sidebar.selectbox("Select Model", ['gpt-3.5-turbo', 'gpt-4-1106-preview'])
-
 # Main chat interface
-st.title("OpenAI Chat")
+st.title("OpenAI Chat with GPT-4")
 
 # Initialize session state variables
 if 'messages' not in st.session_state:
-    st.session_state.messages = []
+    st.session_state['messages'] = []
 
 # Function to send a message to the Assistant
 def send_message(message_text):
-    # Add the user's message to the session state
-    st.session_state.messages.append({'sender': 'User', 'message': message_text})
-    
-    # Create the message for the assistant
-    message = {
-        "role": "user",
-        "content": message_text
-    }
-    
-    # TODO: Here you will handle sending the message to OpenAI and receiving the response
-    # For now, we'll simulate an assistant's response
-    response = "This is a placeholder response."
-    
-    # Add the assistant's response to the session state
-    st.session_state.messages.append({'sender': 'Assistant', 'message': response})
+    if message_text:  # only send a message if it's not empty
+        # Add the user's message to the session state
+        st.session_state['messages'].append({'sender': 'User', 'message': message_text})
+        
+        try:
+            # Create the message for the assistant
+            response = openai.ChatCompletion.create(
+                model="gpt-4-1106-preview",
+                messages=[
+                    {"role": "system", "content": initial_instructions},
+                    {"role": "user", "content": message_text}
+                ]
+            )
+            
+            # Get the assistant's response
+            assistant_message = response.choices[0].message['content']
+
+        except openai.error.OpenAIError as e:
+            assistant_message = f"An error occurred: {str(e)}"
+
+        # Add the assistant's response to the session state
+        st.session_state['messages'].append({'sender': 'Assistant', 'message': assistant_message})
+        
+        # Clear the input box after sending
+        st.session_state['user_message'] = ""
 
 # Input box for the user to type their message
 user_message = st.text_input("Your Message", key="user_message")
 
 # Button to send the message
-if st.button("Send"):
+if st.button("Send") and user_message:
     send_message(user_message)
-    # Clear the input box after sending
-    st.session_state.user_message = ""
 
 # Display chat messages
-for chat in st.session_state.messages:
+for chat in reversed(st.session_state.messages):
     if chat['sender'] == 'User':
-        st.text_area("", value=chat['message'], height=50, key=f"user_{chat['message']}")
+        st.text_area("", value=chat['message'], height=75, key=f"user_{chat['message']}")
     else:
-        st.text_area("", value=chat['message'], height=50, key=f"assistant_{chat['message']}", bg_color="#F0F2F6")
-
-# Run this Streamlit app by typing `streamlit run your_app.py` in your terminal
-
+        st.text_area("", value=chat['message'], height=75, key=f"assistant_{chat['message']}", bg_color="#F0F2F6")
