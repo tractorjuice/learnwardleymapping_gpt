de137d54f9cb9dbeb8db8e4b06f270680f784a15
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-11-06 23:04:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,57 +1,86 @@
+#Importing required packages
 import streamlit as st
 import openai
+import uuid
 
-# Set your OpenAI API key here using Streamlit secrets
+#MODEL = "gpt-3"
+#MODEL = "gpt-3.5-turbo"
+#MODEL = "gpt-3.5-turbo-0613"
+#MODEL = "gpt-3.5-turbo-16k"
+#MODEL = "gpt-3.5-turbo-16k-0613"
+#MODEL = "gpt-4"
+#MODEL = "gpt-4-0314" # Legacy
+#MODEL = "gpt-4-0613"
+#MODEL = "gpt-4-32k-0314" # Legacy
+#MODEL = "gpt-4-32k-0613"
+MODEL = "gpt-4-1106-preview"
+#MODEL = "gpt-4-vision-preview"
+
+if "session_id" not in st.session_state:
+    st.session_state.session_id = str(uuid.uuid4())
+    
+st.set_page_config(page_title="Learn Wardley Mapping")
+st.sidebar.title("Learn Wardley Mapping")
+st.sidebar.divider()
+st.sidebar.markdown("Developed by Mark Craddock](https://twitter.com/mcraddock)", unsafe_allow_html=True)
+st.sidebar.markdown("Current Version: 0.0.1")
+st.sidebar.markdown("Using gpt-4-1106-preview API")
+st.sidebar.markdown(st.session_state.session_id)
+st.sidebar.divider()
+# Check if the user has provided an API key, otherwise default to the secret
+
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-# Define the initial instructions for the assistant
-initial_instructions = "You are a helpful assistant."
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+    st.session_state.messages.append({
+            "role": "system",
+            "content": f"""
+             Interact with LearnWardleyMappingBot, your personal guide to learning and creating Wardley Maps.
+             Discover the power of Wardley Mapping for strategic planning and decision-making by choosing to 'Learn' about the components of a Wardley Map, or 'Vocabulary' and I will provide a list of common terms and their definitions. or 'Create' your own map with step-by-step guidance.
+             If you need assistance, type 'Help' for support. Begin your Wardley Mapping journey now!
+             """
+        })
+    st.session_state.messages.append(   
+        {
+            "role": "user",
+            "content": "Help?"
+        })
+    st.session_state.messages.append(
+        {
+            "role": "assistant",
+            "content": """
+            I'm here to help you learn about and create Wardley Maps. Here are some options for getting started:
+            1. Learn: To learn about the components and concepts of a Wardley Map, type "Learn".
+            2. Vocabulary: To get a list of common Wardley Map terms and their definitions, type "Vocabulary".
+            3. Create: To create your own Wardley Map with step-by-step guidance, type "Create".
+            If you have any specific questions or need clarification on any aspect of Wardley Mapping, feel free to ask.
+            """
+        })
 
-# Main chat interface
-st.title("OpenAI Chat with GPT-4")
+for message in st.session_state.messages:
+    if message["role"] in ["user", "assistant"]:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
-# Initialize session state variables
-if 'messages' not in st.session_state:
-    st.session_state['messages'] = []
+if prompt := st.chat_input("How can I help with Wardley Mapping?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
-# Function to send a message to the Assistant
-def send_message(message_text):
-    if message_text:  # only send a message if it's not empty
-        # Add the user's message to the session state
-        st.session_state['messages'].append({'sender': 'User', 'message': message_text})
-        
-        try:
-            # Create the message for the assistant
-            response = openai.ChatCompletion.create(
-                model="gpt-4-1106-preview",
-                messages=[
-                    {"role": "system", "content": initial_instructions},
-                    {"role": "user", "content": message_text}
-                ]
-            )
-            
-            # Get the assistant's response
-            assistant_message = response.choices[0].message['content']
-
-        except openai.OpenAIError as e:  # Corrected exception handling here
-            assistant_message = f"An error occurred: {str(e)}"
-
-        # Add the assistant's response to the session state
-        st.session_state['messages'].append({'sender': 'Assistant', 'message': assistant_message})
-        
-        # Clear the input box after sending
-        st.session_state['user_message'] = ""
-
-# Input box for the user to type their message
-user_message = st.text_input("Your Message", key="user_message")
-
-# Button to send the message
-if st.button("Send") and user_message:
-    send_message(user_message)
-
-# Display chat messages
-for chat in reversed(st.session_state.messages):
-    if chat['sender'] == 'User':
-        st.text_area("", value=chat['message'], height=75, key=f"user_{chat['message']}")
-    else:
-        st.text_area("", value=chat['message'], height=75, key=f"assistant_{chat['message']}", bg_color="#F0F2F6")
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+            pl_tags=["learnwardleymapping", st.session_state.session_id],
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
