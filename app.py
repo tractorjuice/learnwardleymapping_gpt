8c0177dacf6f6b5f6e6670d2b57667e962f8217b
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-11-07 03:10:41 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,17 @@ import time
 from openai import OpenAI
 client = OpenAI()
 
-#MODEL = "gpt-3"
 #MODEL = "gpt-3.5-turbo"
+#MODEL = "gpt-3.5-turbo-0301"
 #MODEL = "gpt-3.5-turbo-0613"
-#MODEL = "gpt-3.5-turbo-16k"
+#MODEL = "gpt-3.5-turbo-1106"
+MODEL = "gpt-3.5-turbo-16k"
 #MODEL = "gpt-3.5-turbo-16k-0613"
 #MODEL = "gpt-4"
 #MODEL = "gpt-4-0613"
+#MODEL = "gpt-4-0613"
 #MODEL = "gpt-4-32k-0613"
-MODEL = "gpt-4-1106-preview"
+#MODEL = "gpt-4-1106-preview"
 #MODEL = "gpt-4-vision-preview"
 
 if "session_id" not in st.session_state:
