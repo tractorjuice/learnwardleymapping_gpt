0cc75dc08351bd5c73c437713f1a45e97f3c5fac
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-11-07 00:48:24 +0000

    Update app.py

--- app.py
+++ app.py
@@ -56,14 +56,6 @@ st.sidebar.write("Assistant: ", st.session_state.assistant)
 if "thread" not in st.session_state:
     st.session_state.thread = client.beta.threads.create()
 
-if st.session_state.messages:
-    for message in st.session_state.messages.data:
-        if message.role in ["user", "assistant"]:
-            with st.chat_message(message.role):
-                for content_part in message.content:
-                    message_text = content_part.text.value
-                    st.markdown(message_text)
-
 if prompt := st.chat_input("How can I help you?"):
     with st.chat_message('user'):
         st.write(prompt)
@@ -84,3 +76,11 @@ if prompt := st.chat_input("How can I help you?"):
     st.session_state.messages = client.beta.threads.messages.list(
       thread_id=st.session_state.thread.id
     )
+
+if st.session_state.messages:
+    for message in st.session_state.messages.data:
+        if message.role in ["user", "assistant"]:
+            with st.chat_message(message.role):
+                for content_part in message.content:
+                    message_text = content_part.text.value
+                    st.markdown(message_text)
